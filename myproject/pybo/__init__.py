51f48b417cd03c85f0889d270d2ad5c4dbca135d
Fillsogood <jdd05273@naver.com>
2024-01-04 17:07:47 +0900

    migrations로 데이터베이스 구축

--- myproject/pybo/__init__.py
+++ myproject/pybo/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 # 플라스크 애플리케이션을 생성하는 코드. name이라는 변수에 pybo.py 모듈이 담겨 실행
 #name은 pybo라는 문자열이 담긴다. @app.route는 URL과 플라스크 코드를 매핑하는 데코레이터다
@@ -6,8 +13,15 @@ from flask import Flask
 
 def create_app(): #create app함수가 app 객체를 생성해 반환하도록 수정 이 함수가 애플리케이션 팩토리이다.
     app = Flask(__name__)
+    app.config.from_object(config)
 
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from . import models
+
+    # 블루프린트
     from .views import main_views
     app.register_blueprint(main_views.bp)
-    
+
     return app
--- myproject/pybo/views/main_views.py
+++ myproject/pybo/views/main_views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-
+#creat app 함수안에 매핑이 필요할 때마다 너무 길어져서 블루프린트로 체계적으로 관리
 bp = Blueprint('main',__name__, url_prefix='/') #main은 bp의 별칭
 
 @bp.route('/hello')
